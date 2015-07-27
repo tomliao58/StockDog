6bbb8c5a257fc6befa38d3288821eed97eb37f5d
tomliao58@gmail.com <cbb/38;oenny>
2015-07-27 14:57:03 +0800

    Sample app for monitoring stock price changes
    Sample app from the book, Professional AngularJS, published by Wrox

---
+++ app/scripts/directives/stk-stock-table.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockDogApp.directive:stkStockTable
+ * @description
+ * # stkStockTable
+ */
+angular.module('stockDogApp')
+  .directive('stkStockTable', function () {
+      return {
+          templateUrl: 'views/templates/stock-table.html',
+          restrict: 'E',
+          // [1] Isolate scope: 如此可讓程式直接繫結(bind)一個 directive 的 DOM element
+          scope: { watchlist: '=' },
+          // [2] Create a controller, which serves as an API for this directive
+          controller: function ($scope) {
+              var rows = [];
+
+              $scope.$watch('showPercent', function (showPercent) {
+                  if (showPercent) {
+                      _.each(rows, function (row) {
+                          row.showPercent = showPercent;
+                      });
+                  }
+              });
+
+              this.addRow = function (row) {
+                  rows.push(row);
+              };
+
+              this.removeRow = function (row) {
+                  _.remove(rows, row);
+              };
+          },
+          // [3] Standard link function implementation
+          link: function ($scope) {
+              $scope.showPercent = false;
+              $scope.removeStock = function (stock) {
+                  $scope.watchlist.removeStock(stock);
+              };
+          }
+      };
+  });
---
+++ app/scripts/services/quote-service.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name stockDogApp.QuoteService
+ * @description
+ * # QuoteService
+ * Service in the stockDogApp.
+ */
+angular.module('stockDogApp')
+  .service('QuoteService', function ($http, $interval) {
+      // AngularJS will instantiate a singleton by calling "new" on this function
+      var stocks = [];
+      var BASE = 'http://query.yahooapis.com/v1/public/yql';
+
+      // [1] Handles updating stock model with appropriate data from quote
+      var update = function (quotes) {
+          //console.log(quotes);
+          if (quotes.length === stocks.length) {
+              _.each(quotes, function (quote, idx) {
+                  var stock = stocks[idx];
+                  stock.lastPrice = parseFloat(quote.LastTradePriceOnly);
+                  //stock.lastPrice = parseFloat(quote.LastTradePriceOnly) + _.random(-0.5, 0.5); //若美國股市休市中，以此行程式測試價格變動時顏色 animation 的效果
+                  stock.change = quote.Change;
+                  stock.percentChange = quote.ChangeinPercent;
+                  stock.marketValue = stock.shares * stock.lastPrice;
+                  stock.dayChange = stock.shares * parseFloat(stock.change);
+                  stock.save();
+              });
+          }
+      };
+
+      // [2] Helper functions for managing which stocks to pull quotes for
+      this.register = function (stock) {
+          stocks.push(stock);
+      };
+      this.deregister = function (stock) {
+          _.remove(stocks, stock);
+      };
+      this.clear = function () {
+          stocks = [];
+      };
+
+      // [3] Main processing function for communicating with Yahoo Finance API
+      this.fetch = function () {
+          var symbols = _.reduce(stocks, function (symbols, stock) {
+              symbols.push(stock.company.symbol);
+              return symbols;
+          }, []);
+          var query = encodeURIComponent('select * from yahoo.finance.quotes ' +
+              'where symbol in (\'' + symbols.join(',') + '\')');
+          var url = BASE + '?' + 'q=' + query + '&format=json&diagnostics=true' +
+              '&env=http://datatables.org/alltables.env';
+          $http.jsonp(url + '&callback=JSON_CALLBACK')
+              .success(function (data) {
+                  if (data.query.count) {
+                      var quotes = data.query.count > 1 ? data.query.results.quote : [data.query.results.quote];
+                      update(quotes);
+                  }
+              })
+              .error(function (data) {
+                  console.log(data);
+              });
+      };
+
+      // [4] Used to fetch new quote data every 5 seconds
+      $interval(this.fetch, 5000);
+
+  });
